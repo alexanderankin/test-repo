05288423d129dbd6537628153d499f18973b00bf
Toor <toor@z14-1>
2024-01-03 11:57:56 -0500

    testing-stuff

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
+
+IDictionary<string, int> a = new Dictionary<string, int>();
+
+a.Add("abc", 1);
+
+foreach (var i in a)
+{
+    Console.WriteLine(i.Key + ": " + i.Value);
+}
+
+int total = 10;
+new Random().Next(0, total);
