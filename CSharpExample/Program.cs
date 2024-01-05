439a68c8ffc183b83b8d011052de5a4be55f9def
Toor <toor@z14-1>
2024-01-05 12:07:33 -0500

    add examples for SelectToken

--- CSharpExample/Program.cs
+++ CSharpExample/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Newtonsoft.Json.Linq;
+
 static void Program()
 {
     IDictionary<string, int> a = new Dictionary<string, int>();
@@ -39,4 +41,31 @@ static object SomeMethod()
     }
 
     return null;
+}
+
+Dictionary<string, Dictionary<string, Dictionary<string, string>>> item =
+    new Dictionary<string, Dictionary<string, Dictionary<string, string>>>
+    {
+        {
+            "outer.1", new Dictionary<string, Dictionary<string, string>>
+            {
+                { "outer-2", new Dictionary<string, string> { { "inner", "value" } } }
+            }
+        }
+    };
+
+var jObjectFromObject = JObject.FromObject(item);
+var jObjectFromString =
+    JObject.Parse(
+        "{\"outer.1\":{\"outer-2\": {\"inner\":\"value\"}}}"
+    );
+
+foreach (var jObject in new List<JObject> { jObjectFromObject, jObjectFromString })
+{
+    // Console.WriteLine(item.GetValueOrDefault("outer.1", null)?.GetValueOrDefault("outer-2-DNE", null)?.GetValueOrDefault("inner", null));
+    // Console.WriteLine(item["outer.1"]["outer-2-DNE"]?["inner"]);
+
+    Console.WriteLine("works: " + jObject.SelectToken("$.['outer.1'].['outer-2'].['inner']")?.ToString());
+    Console.WriteLine("works: " + jObject.Value<JObject>("outer.1")?.Value<JObject>("outer-2")?.GetValue("inner")?.ToString());
+    Console.WriteLine("not working: " + jObject.Value<JObject>("outer.1")?.Value<JObject>("outer.DNE")?.GetValue("inner")?.ToString());
 }
