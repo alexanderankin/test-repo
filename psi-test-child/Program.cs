7ebcfb68be33adde3c4a991975c1b5f3e4e9d1c2
David Ankin <daveankin@gmail.com>
2025-10-25 13:15:57 -0400

    background process works

---
+++ psi-test-child/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+string path = Path.Combine(Environment.CurrentDirectory, "output.txt");
+Console.WriteLine($"Child running. Logging to {path}");
+
+while (true)
+{
+    string line = $"hello {DateTime.Now:O}";
+    File.AppendAllText(path, line + Environment.NewLine);
+    Thread.Sleep(1000);
+}
---
+++ psi-test-main/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+string exe = "psi-test-child\\bin\\Debug\\net9.0\\psi-test-child.exe";
+
+var psi = new ProcessStartInfo
+{
+    FileName = exe,
+    UseShellExecute = false,
+    CreateNoWindow = true,
+};
+
+if (OperatingSystem.IsWindows())
+    psi.WindowStyle = ProcessWindowStyle.Hidden;
+
+var proc = Process.Start(psi)!;
+Console.WriteLine($"Launched child process with PID {proc.Id}");
+Console.WriteLine("Parent exiting.");
